7f0d8cbe3758ff70308a43661891f79c50407918
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-10-08 20:28:09 +0530

    Intialising method of array

---
+++ Array_2.java
@@ -0,0 +1,16 @@
+package Basic_Java;
+
+public class Array_2 {
+
+	public static void main(String[] args) {
+		//type 2 of defining array.
+		
+		int marks[] = {45,84,95,45,15};
+		
+		for(int i =0 ; i<marks.length ; i++) {
+			System.out.println(marks[i]);
+		}
+
+	}
+
+}
