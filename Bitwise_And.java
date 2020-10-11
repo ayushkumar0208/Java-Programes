9d7733c351f4807a89712041750ad5110a91a223
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-10-11 12:59:03 +0530

    Bitwise and

---
+++ Bitwise_And.java
@@ -0,0 +1,11 @@
+package Basic_Java;
+public class Bitwise_And {
+	public static void main(String[]args) {
+		int a = 4;
+		int b = 6;
+		//Use binary numbers for a & b  
+		int c = a & b;
+		System.out.println(c);
+	}
+
+}
