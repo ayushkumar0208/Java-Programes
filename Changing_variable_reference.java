71c8d93f5726af53750ef3230db8c4be8f8f2c2d
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-10-14 18:21:58 +0530

    Changing variable reference

---
+++ Changing_variable_reference.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Changing_variable_reference {
+
+	public static void main(String[] args) {
+		String name = "Ayush";
+		name = "Kumar";
+		System.out.println(name);
+
+	}
+
+}
