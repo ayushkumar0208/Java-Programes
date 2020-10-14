710f37d081c0c713d64cea43eb7c160cdea7180b
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-10-14 18:22:39 +0530

    ChatAt

---
+++ charAt.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class charAt {
+
+	public static void main(String[] args) {
+		String name = "Ayush";
+		char value = name.charAt(4);
+		System.out.println(value);
+
+	}
+
+}
