9b4cea5faba16dbc62149fbd94835d585992e20a
Ayush Kumar <aryaayushgithub0208@gmail.com>
2020-10-21 22:25:33 +0530

    Method Over Loading

---
+++ Method_overloading.java
@@ -0,0 +1,25 @@
+package Basic_Java;
+
+public class Method_overloading {
+
+	public static void main(String[] args) {
+		//when a class two or more methods by same name but different parameters , it is known as method overloading.
+		//for example:
+		
+		
+		// int maxof(int a, int b){
+		//
+		// }
+		
+		// int maxof(float a, float b, float c){
+		//
+		// }
+		
+		// int maxof(int a,float b){
+		// 
+		// }
+		
+
+	}
+
+}
