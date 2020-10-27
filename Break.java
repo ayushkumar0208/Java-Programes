ef1a61c49fa6e0627cbad61dee206526e680be51
Ayush Kumar <71185961+ayushkumar0208@users.noreply.github.com>
2020-10-27 22:42:13 +0530

    Break function

---
+++ Break.java
@@ -0,0 +1,16 @@
+package Basic_Java;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i<=100;i++) {
+			if(i==35) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
